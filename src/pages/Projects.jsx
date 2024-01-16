e09b2d796dbd44758da202e69ab33deda13ce73f
Kartik Bhardwaj <developer.kartikbhardwaj@gmail.com>
2024-01-16 08:53:07 +0530

    some updates in project page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,11 +1,13 @@
-import React, { Suspense, useEffect, useState } from "react";
+import React, { Suspense, useEffect, useRef, useState } from "react";
 import { motion } from "framer-motion";
 import Links from "../components/Links";
 import project from "../assets/project.png";
+import Coinstein from "../assets/Coinstein.png";
+import notes from "../assets/notes.png";
+import bookish from "../assets/bookish.png";
 import github from "../assets/icons/github.png";
 import redirect from "../assets/icons/redirect.png";
 import scrollImg from "../assets/icons/scroll.gif";
-
 const Projects = () => {
   const [scrollerVisibility, setScrollerVisibility] = useState("visible");
   var limitRenders = 1;
@@ -15,6 +17,9 @@ const Projects = () => {
       setScrollerVisibility("hidden");
     }
   });
+
+  const mouseRef = useRef();
+
   return (
     <div className="container" onScroll={() => console.log("scrolled")}>
       <Links name="Projects" />
@@ -25,11 +30,54 @@ const Projects = () => {
           void of any plagiarism or replication.&#41;
         </h6>
         <div className="project-card-wrapper">
-          <ProjectCard cardPosition={"start"} />
-          <ProjectCard cardPosition={"end"} />
-          <ProjectCard cardPosition={"start"} />
+          <ProjectCard
+            cardPosition={"start"}
+            title="Bombay Estates"
+            desc="Bombay Estates is a real estate website that provides a user
+            friendly interface to businesses and individuals who are interested
+            in buying, selling and even renting the real estate properties."
+            git="https://github.com/DevKartikBhardwaj/bombayestates"
+            live="https://bombayestates-bkta86j4k-devkartikbhardwaj.vercel.app/"
+            img={project}
+            imgClass={"img1"}
+          />
+          <ProjectCard
+            cardPosition={"end"}
+            title="Coinstein"
+            desc="A cryptocurrency data provider website is a platform that offers
+             a wide range of data and information related to cryptocurrencies. These 
+             websites are valuable resources for traders or anyone interested in cryptocurrencies."
+            git="https://github.com/DevKartikBhardwaj/coinstein"
+            live="https://coinstein.vercel.app/"
+            img={Coinstein}
+            imgClass={"img2"}
+          />
+          <ProjectCard
+            cardPosition={"start"}
+            title="Notes"
+            desc="A cryptocurrency data provider website is a platform that offers
+             a wide range of data and information related to cryptocurrencies. These 
+             websites are valuable resources for traders or anyone interested in cryptocurrencies."
+            git="https://github.com/DevKartikBhardwaj/notes-client"
+            live="https://notes-client-rho.vercel.app/"
+            img={notes}
+            imgClass={"img3"}
+          />
+          <ProjectCard
+            cardPosition={"start"}
+            title="Bookish"
+            desc="A cryptocurrency data provider website is a platform that offers
+             a wide range of data and information related to cryptocurrencies. These 
+             websites are valuable resources for traders or anyone interested in cryptocurrencies."
+            git="https://github.com/DevKartikBhardwaj/bookish"
+            live="https://bookish-8avz.onrender.com/"
+            img={bookish}
+            imgClass={"img4"}
+          />
+          {/* <ProjectCard cardPosition={"end"} />
+          <ProjectCard cardPosition={"start"} /> */}
           {/* <motion.div initial="hidden" whileInView="visible"> */}
-          <ProjectCard cardPosition={"end"} />
+          {/* <ProjectCard cardPosition={"end"} /> */}
           {/* </motion.div> */}
         </div>
       </div>
@@ -42,7 +90,15 @@ const Projects = () => {
   );
 };
 
-const ProjectCard = ({ cardPosition }) => {
+const ProjectCard = ({
+  cardPosition,
+  title,
+  desc,
+  git,
+  live,
+  img,
+  imgClass,
+}) => {
   return (
     <motion.div
       initial="hidden"
@@ -56,21 +112,17 @@ const ProjectCard = ({ cardPosition }) => {
     >
       <div className="project-card">
         <div className="img-half">
-          <img src={project} alt="bomabay estates" />
+          <img src={img} alt="bomabay estates" className={imgClass} />
         </div>
         <div className="project-description-half">
-          <h2>Bombay Estates</h2>
-          <p>
-            Bombay Estates is a real estate website that provides a user
-            friendly interface to businesses and individuals who are interested
-            in buying, selling and even renting the real estate properties.
-          </p>
+          <h2>{title}</h2>
+          <p>{desc}</p>
           <div className="links-container">
-            <a href="">
+            <a href={git} target="_blank">
               Source Code &nbsp;
               <img src={github} alt="source code on github" height={"22px"} />
             </a>
-            <a href="">
+            <a href={live} target="_blank">
               Live Site &nbsp;
               <img src={redirect} alt="link to live site" height={"20px"} />
             </a>
