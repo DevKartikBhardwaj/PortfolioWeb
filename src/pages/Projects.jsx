3ffa2cbf97471eb1593386779d07f9dab230c1a6
Kartik Bhardwaj <102840713+DevKartikBhardwaj@users.noreply.github.com>
2024-09-28 19:31:30 +0530

    Update Projects.jsx

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -75,7 +75,7 @@ const Projects = () => {
           <ProjectCard
             cardPosition={"start"}
             title="Chatease"
-            desc="Chatease is a versatile chatting application designed for seamless two-way communication, complemented by features for single chat and group creation, fostering collaborative interaction among its diverse user base."
+            desc="Chatease is a versatile chatting application designed for seamless two-way communication, complemented by features for personal chats and group creation."
             git="https://github.com/DevKartikBhardwaj/Chatease"
             live="https://chatease-frontend-4ips.onrender.com/"
             img={"https://res.cloudinary.com/dwhf5f08p/image/upload/v1727530344/screencapture-chatease-frontend-4ips-onrender-2024-09-28-19_02_00_odautq.png"}
